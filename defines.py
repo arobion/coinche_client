95074b2ace760d8d4dee7b984e256018a56f23d5
Robion Adrien <arobion@student.42.fr>
2020-07-30 15:14:44 +0200

    minor GUI features

--- defines.py
+++ defines.py
@@ -6,6 +6,7 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 FRAMERATE = 30
 TIME_SCORE = 1
+TIME_PLI = 1
 
 ATOUT = ["J", "9", "AS", "10", "K", "Q", "8", "7"]
 NORMAL = ["AS", "10", "K", "Q", "J", "9", "8", "7"]
